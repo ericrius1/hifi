ee2cc69295882fbb00a0f53c209be6b5bde8dfbd
ericrius1 <ericrius1>
2016-01-12 14:28:04 -0800

    merged
    les/controllers/modules/stickyStuff.js

--- examples/controllers/modules/stickyStuff.js
+++ examples/controllers/modules/stickyStuff.js
@@ -22,7 +22,6 @@ function StickyStuffManager() {
 
     this.handleReleaseMessage = function(entityID) {
         this.releasedEntity = entityID;
-<<<<<<< HEAD
         this.releasedEntityProps = Entities.getEntityProperties(this.releasedEntity, ["dimensions", "position", "parentID"]);
         this.releasedEntityParentID = this.releasedEntityProps.parentID;
         var searchBox = Vec3.multiply(this.releasedEntityProps.dimensions, 2);
@@ -34,21 +33,7 @@ function StickyStuffManager() {
                     parentID: entity,
                     collisionsWillMove: false,
                     parentJointIndex: 0,
-=======
-        var props = Entities.getEntityProperties(this.releasedEntity, ["dimensions", "position", "parentID"]);
-        this.releasedEntityParentID = props.parentID;
-        var searchBox = Vec3.multiply(props.dimensions, 2);
-        var intersectedEntities = Entities.findEntitiesInBox(props.position, searchBox);
-        for (var i = 0; i < intersectedEntities.length; i++) {
-            var entity = intersectedEntities[i];
-            if (JSON.stringify(entity) !== JSON.stringify(this.releasedEntity) && this.entityIsSticky(entity) ) {
-                var name = Entities.getEntityProperties(entity, "name").name;
-                print("Found a sticky Entity! " + name);
-                Entities.editEntity(this.releasedEntity, {
-                    parentID: entity,
-                    collisionsWillMove: false,
-                    // ignoreForCollisions: true,
->>>>>>> origin/parentingStuff
+
                     velocity: {
                         x: 0,
                         y: 0,
@@ -59,37 +44,17 @@ function StickyStuffManager() {
                         y: 0,
                         z: 0
                     },
-<<<<<<< HEAD
                     angularVelocity: {
                         x: 0,
                         y: 0,
                         z: 0
                     }
                 });
-=======
-                    angularVelocity: {x: 0, y: 0, z: 0}
-                });
-                // Entities.editEntity(entity, {
-                //     velocity: {
-                //         x: 0,
-                //         y: 0,
-                //         z: 0
-                //     },
-                //     gravity: {
-                //         x: 0,
-                //         y: 0,
-                //         z: 0
-                //     },
-                //     angularVelocity: {x: 0, y: 0, z:0},
-                //     ignoreForCollisions: true
-                // });
->>>>>>> origin/parentingStuff
-                return;
+
             }
         }
-        print("NO VALID INTERSECTED ENTITIES");
     }
-<<<<<<< HEAD
+
     this.entityShouldStick = function(entity) {
         var props = Entities.getEntityProperties(entity, ["type", "dimensions", "position", "locked", "parentID"]);
         if (props.type !== "Model" && props.type !== "Box" && props.type !== "Sphere") {
@@ -108,41 +73,22 @@ function StickyStuffManager() {
         //     print("TESTED ENTITY ALREADY HAS A PARENT")
         //     return false;
         // }
-        
+
         //Ok- this entity is sticky- now we just need to make sure the two entities intersect!
         if (props.position.x + props.dimensions.x / 2 < this.releasedEntityProps.position.x - this.releasedEntityProps.dimensions.x / 2 || props.position.x - props.dimensions.x / 2 > this.releasedEntityProps.position.x + this.releasedEntityProps.dimensions.x / 2) {
             // The left edge of bounding box A is greater than the right edge of box b, so there's no way they intersect
-            print("X AXIS DOESNT INTERSECT")
+            print("X AXIS DOESNT INTERSECT!")
             return false;
         }
         if (props.position.y + props.dimensions.y / 2 < this.releasedEntityProps.position.y - this.releasedEntityProps.dimensions.y / 2 || props.position.y - props.dimensions.y / 2 > this.releasedEntityProps.position.y + this.releasedEntityProps.dimensions.y / 2) {
             // The left edge of bounding box A is greater than the right edge of box b, so there's no way they intersect
-            print("Y AXIS DOESNT INTERSECT")
+            print("Y AXIS DOESNT INTERSECT!")
             return false;
         }
         if (props.position.z + props.dimensions.z / 2 < this.releasedEntityProps.position.y - this.releasedEntityProps.dimensions.y / 2 || props.position.y - props.dimensions.y / 2 > this.releasedEntityProps.position.y + this.releasedEntityProps.dimensions.y / 2) {
             // The left edge of bounding box A is greater than the right edge of box b, so there's no way they intersect
+            print("Z AXIS DOESNT INTERSECT!")
             return false;
-=======
-    this.entityIsSticky = function(entity) {
-        var props = Entities.getEntityProperties(entity, ["type", "dimensions", "locked", "parentID"]);
-        if (props.type === "Model" || props.type === "Box" || props.type === "Sphere") {
-            if (props.locked) {
-                return false;
-            }
-            if (props.type === "Model" && Vec3.length(props.dimensions) > this.maxDimensionLength) {
-                // If dimensions are huge, this is probably not an entity we want to make sticky
-                return false;
-            }
-
-            if ( JSON.stringify(props.parentID) !== JSON.stringify(this.releasedEntityParentID  )) {
-               // If the entity we are testing already has a parent, we don't add our released entity as a child of it
-                print("TESTED ENTITY ALREADY HAS A PARENT")
-                return false;
-            }
-          
-            return true;
->>>>>>> origin/parentingStuff
         }
 
         return true;
--- examples/testParenting.js
+++ examples/testParenting.js
@@ -3,18 +3,52 @@ orientation = Quat.safeEulerAngles(orientation);
 orientation.x = 0;
 orientation = Quat.fromVec3Degrees(orientation);
 var center = Vec3.sum(MyAvatar.position, Vec3.multiply(3, Quat.getFront(orientation)));
-var currentDimensions = {x: 1, y: 1, z: 1};
+var currentDimensions = {
+    x: 0.7,
+    y: 0.7,
+    z: 0.7
+};
 
 var block1 = Entities.addEntity({
     type: "Box",
-    dimensions: {x: 1, y: 1, z: 1},
+    dimensions: currentDimensions,
     position: center,
-    color: {red: 200, green: 10, blue: 10}
+    color: {
+        red: 200,
+        green: 10,
+        blue: 10
+    },
+    collisionsWillMove: true,
+    userData: JSON.stringify({
+        grabbableKey: {
+            invertSolidWhileHeld: true
+        }
+    })
 });
+
+currentDimensions = Vec3.multiply(currentDimensions, 0.6);
+var block2 = Entities.addEntity({
+    type: "Box",
+    dimensions: currentDimensions,
+    position: center,
+    color: {
+        red: 0,
+        green: 200,
+        blue: 10
+    },
+    collisionsWillMove: true,
+    userData: JSON.stringify({
+        grabbableKey: {
+            invertSolidWhileHeld: true
+        }
+    })
+});
+
 
 
 function cleanup() {
     Entities.deleteEntity(block1);
+    Entities.deleteEntity(block2);
 }
 
 Script.scriptEnding.connect(cleanup);
