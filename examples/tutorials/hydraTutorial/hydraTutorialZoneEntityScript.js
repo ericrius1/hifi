eb509802073bb82700268670ca586e3570785700
ericrius1 <ericrius1>
2016-01-27 13:45:48 -0800

    got right hand in that right spot

--- examples/tutorials/hydraTutorial/hydraTutorialZoneEntityScript.js
+++ examples/tutorials/hydraTutorial/hydraTutorialZoneEntityScript.js
@@ -10,27 +10,64 @@
 //
 
 (function() {
-    Script.include("../libraries/utils.js");
+    Script.include("..//..libraries/utils.js");
     var _this;
-   
+
     var HydraTutorialZone = function() {
         _this = this;
+        this.RIGHT_HAND = 1;
+        this.LEFT_HAND = 0;
+        this.RIGHT_HYDRA_MODEL_URL = "https://s3-us-west-1.amazonaws.com/hifi-content/eric/models/Hydra-Tips-RIGHT.fbx";
+        this.LEFT_HYDRA_MODEL_URL = "https://s3-us-west-1.amazonaws.com/hifi-content/eric/models/Hydra-Tips-LEFT.fbx";
+        this.RIGHT_HAND_JOINT_INDEX = MyAvatar.getJointIndex("RightHand");
+        this.LEFT_HAND_JOINT_INDEX = MyAvatar.getJointIndex("LeftHand");
     };
 
     HydraTutorialZone.prototype = {
 
         enterEntity: function() {
-            print("ENTITY ENTERED");
+            print("EBL ENTITY ENTERED");
+            this.placeControllersInHand();
+        },
+
+        placeControllersInHand: function() {
+            this.rightHandHydraModel = Entities.addEntity({
+                type: "Model",
+                name: "Right Hydra",
+                modelURL: this.RIGHT_HYDRA_MODEL_URL,
+                parentID: MyAvatar.sessionUUID,
+                parentJointIndex: this.RIGHT_HAND_JOINT_INDEX,
+                rotation: Quat.fromPitchYawRollDegrees(47, 15, 70),
+                position: Vec3.sum(MyAvatar.getRightHandPosition(), {x: -0.05, y: 0.07, z: -0.02})
+            });
+s
+            this.leftHandHydraModel = Entities.addEntity({
+                type: "Model",
+                name: "Left Hydra",
+                modelURL: this.LEFT_HYDRA_MODEL_URL,
+                parentID: MyAvatar.sessionUUID,
+                parentJointIndex: this.LEFT_HAND_JOINT_INDEX
+            });
+
         },
 
         leaveEntity: function() {
-            print("LEAVE ENTITY");
+            print("EBL LEAVE ENTITY");
+            this.cleanup();
         },
 
         preload: function(entityID) {
             this.entityID = entityID;
-            print("HEYO");
         },
+
+        cleanup: function() {
+            Entities.deleteEntity(this.rightHandHydraModel);
+            Entities.deleteEntity(this.leftHandHydraModel);
+        },
+
+        unload: function() {
+            this.cleanup();
+        }
     };
     return new HydraTutorialZone();
 });
--- examples/tutorials/hydraTutorial/hydraTutorialZoneSpawner.js
+++ examples/tutorials/hydraTutorial/hydraTutorialZoneSpawner.js
@@ -1,5 +1,3 @@
-  var shnur = "bdur"
-
   var orientation = Camera.getOrientation();
   orientation = Quat.safeEulerAngles(orientation);
   orientation.x = 0;
@@ -8,18 +6,38 @@
 
   var SCRIPT_URL = Script.resolvePath("hydraTutorialZoneEntityScript.js?v1" + Math.random());
 
-  var box = Entities.addEntity({
+  var MODEL_URL = "https://hifi-content.s3.amazonaws.com/alan/dev/Holographic-Stage-no-roof.fbx"
+  var tutorialZone = Entities.addEntity({
+      type: "Model",
+      name: "Tutorial Zone",
+      modelURL: MODEL_URL,
+      position: {
+          x: 532.03,
+          y: 495.58,
+          z: 518.95
+      },
+      dimensions: {
+          x: 5.1,
+          y: 2.58,
+          z: 5.1
+      },
+      rotation: Quat.fromPitchYawRollDegrees(0, 200, 0)
+  });
+
+
+
+  var triggerBox = Entities.addEntity({
       type: "Box",
       color: {
           red: 200,
           green: 10,
           blue: 200
       },
-      position: center,
+      parentID: tutorialZone,
       dimensions: {
-          x: 5,
-          y: 5,
-          z: 5
+          x: 0.8,
+          y: 0.8,
+          z: 0.8
       },
       collisionless: true,
       script: SCRIPT_URL
@@ -27,7 +45,8 @@
 
 
   function cleanup() {
-    Entities.deleteEntity(box);
+      Entities.deleteEntity(triggerBox);
+      Entities.deleteEntity(tutorialZone);
   }
 
 
