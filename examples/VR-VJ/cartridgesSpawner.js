8c8820163c31d075510be45b48b42f731d6d24be
ericrius1 <ericrius1>
2016-03-13 07:44:18 -0700

    emissive maps

--- examples/VR-VJ/cartridgesSpawner.js
+++ examples/VR-VJ/cartridgesSpawner.js
@@ -4,12 +4,7 @@
   orientation = Quat.fromVec3Degrees(orientation);
   var center = Vec3.sum(MyAvatar.getHeadPosition(), Vec3.multiply(2, Quat.getFront(orientation)));
 
-  var cartridgeSpawnPosition = {
-    x: 203.4,
-    y: 1,
-    z: 351.3
-  };
-
+  var CARTRIDGE_ROTATION = Quat.fromPitchYawRollDegrees(0, 0, 0);
 
   Script.include("../libraries/utils.js");
 
@@ -21,7 +16,103 @@
   spawnSoundCartridges();
   spawnVisualCartridges();
 
-  function spawnSkybox() {
+
+
+  function spawnSoundCartridges() {
+    var SOUND_SCRIPT_URL = Script.resolvePath("VRVJSoundCartridgeEntityScript.js");
+    var SOUND_CARTRIDGE_NAME = "VRVJ-Sound-Cartridge";
+    var soundCartridgeUserData = {
+      soundURL: "https://s3-us-west-1.amazonaws.com/hifi-content/eric/Sounds/VRVJ/sunrise_ambient.wav",
+    };
+    var soundCartridgeProps = {
+      type: "Model",
+      modelURL: "https://s3-us-west-1.amazonaws.com/hifi-content/jazmin/dev/_vrhackathon/musiccartridge_violet.fbx",
+      shapeType: "sphere",
+      name: SOUND_CARTRIDGE_NAME,
+      dimensions: {
+        x: 0.3404,
+        y: 0.3404,
+        z: 0.0833
+      },
+      position:  {x: 207.1613, y: 0.057, z: 351.3485},
+      rotation: CARTRIDGE_ROTATION,
+      damping: 1,
+      angularDamping: 1,
+      collidesWith: "",
+      dynamic: true,
+      script: SOUND_SCRIPT_URL,
+      userData: JSON.stringify(soundCartridgeUserData)
+    }
+    soundCartridges.push(Entities.addEntity(soundCartridgeProps));
+
+
+    soundCartridgeUserData.soundURL = "https://s3-us-west-1.amazonaws.com/hifi-content/eric/Sounds/VRVJ/tribaldrums.wav";
+    soundCartridgeProps.modelURL = "https://s3-us-west-1.amazonaws.com/hifi-content/jazmin/dev/_vrhackathon/musiccartridge_teal.fbx";
+    soundCartridgeProps.position =  {x:203.9634, y: 0.057, z: 351.5041};
+    soundCartridgeProps.userData = JSON.stringify(soundCartridgeUserData)
+    soundCartridges.push(Entities.addEntity(soundCartridgeProps));
+
+    soundCartridgeUserData.soundURL = "https://s3-us-west-1.amazonaws.com/hifi-content/eric/Sounds/VRVJ/rain.wav?v3";
+    soundCartridgeProps.modelURL = "https://s3-us-west-1.amazonaws.com/hifi-content/jazmin/dev/_vrhackathon/musiccartridge_pink.fbx";
+    soundCartridgeProps.userData = JSON.stringify(soundCartridgeUserData)
+    soundCartridgeProps.position = {
+      x: 205.4914,
+      y: 0.057,
+      z: 351.43
+    };
+    soundCartridges.push(Entities.addEntity(soundCartridgeProps));
+  }
+
+  function spawnVisualCartridges() {
+    var visualCartridgeScriptURL = Script.resolvePath("visualCartridgeEntityScripts/firePoiVisualCartridgeEntityScript.js?v1" + Math.random());
+
+    var visualCartridgeProps = {
+      type: "Model",
+      modelURL: "https://s3-us-west-1.amazonaws.com/hifi-content/jazmin/dev/_vrhackathon/torch_cartridge.fbx",
+      shapeType: "sphere",
+      name: "VRVJ-Visual-Cartridge",
+      dimensions: {
+        x: 0.2355,
+        y: 0.2355,
+        z: 0.0187
+      },
+      rotation:CARTRIDGE_ROTATION,
+      damping: 1,
+      collidesWith: "",
+      angularDamping: 1,
+      color: {
+        red: 0,
+        green: 200,
+        blue: 10
+      },
+      dynamic: true,
+      position: {x:203.9634, y: 0.67, z: 351.5041},
+      script: visualCartridgeScriptURL,
+    }
+    visualCartridges.push(Entities.addEntity(visualCartridgeProps));
+
+    visualCartridgeProps.script = Script.resolvePath("visualCartridgeEntityScripts/reactiveSkybox/reactiveSkyboxSunVisualCartridgeEntityScript.js?v1" + Math.random());
+    var cartridgeUserData = {};
+    cartridgeUserData.reactiveSkybox = VRVJSkybox;
+    visualCartridgeProps.userData = JSON.stringify(cartridgeUserData);
+    visualCartridgeProps.position = {x: 207.1613, y: 0.68, z: 351.3485};
+    visualCartridgeProps.modelURL = "https://s3-us-west-1.amazonaws.com/hifi-content/jazmin/dev/_vrhackathon/sun_cartridge.fbx";
+    visualCartridges.push(Entities.addEntity(visualCartridgeProps));
+
+
+    visualCartridgeProps.script = Script.resolvePath("visualCartridgeEntityScripts/reactiveSkybox/reactiveSkyboxRainVisualCartridgeEntityScript.js?v1" + Math.random());
+    visualCartridgeProps.modelURL = "https://s3-us-west-1.amazonaws.com/hifi-content/jazmin/dev/_vrhackathon/rain_cartridge.fbx";
+    visualCartridgeProps.position = {
+      x: 205.4914,
+      y: 0.67,
+      z: 351.43
+    };
+    visualCartridges.push(Entities.addEntity(visualCartridgeProps));
+
+
+  }
+
+    function spawnSkybox() {
     var SKYBOX_SHADER_URL = Script.resolvePath("visualCartridgeEntityScripts/reactiveSkybox/rainyDayNightSkybox.fs");
     var position = MyAvatar.position;
     var skyboxUserData = {
@@ -122,94 +213,6 @@
       },
       rotation: Quat.fromPitchYawRollDegrees(0, -22.5, 0)
     });
-
-
-  }
-
-  function spawnSoundCartridges() {
-    var SOUND_SCRIPT_URL = Script.resolvePath("VRVJSoundCartridgeEntityScript.js");
-    var SOUND_CARTRIDGE_NAME = "VRVJ-Sound-Cartridge";
-    var soundCartridgeUserData = {
-      soundURL: "https://s3-us-west-1.amazonaws.com/hifi-content/eric/Sounds/VRVJ/sunrise_ambient.wav",
-    };
-    var soundCartridgeProps = {
-      type: "Box",
-      name: SOUND_CARTRIDGE_NAME,
-      dimensions: {
-        x: 0.1,
-        y: 0.1,
-        z: 0.1
-      },
-      color: {
-        red: 200,
-        green: 10,
-        blue: 200
-      },
-      position: cartridgeSpawnPosition,
-      damping: 1,
-      angularDamping: 1,
-      collidesWith: "",
-      dynamic: true,
-      script: SOUND_SCRIPT_URL,
-      userData: JSON.stringify(soundCartridgeUserData)
-    }
-    soundCartridges.push(Entities.addEntity(soundCartridgeProps));
-
-
-    soundCartridgeUserData.soundURL = "https://s3-us-west-1.amazonaws.com/hifi-content/eric/Sounds/VRVJ/tribaldrums.wav";
-    soundCartridgeProps.userData = JSON.stringify(soundCartridgeUserData)
-    soundCartridges.push(Entities.addEntity(soundCartridgeProps));
-
-    soundCartridgeUserData.soundURL = "https://s3-us-west-1.amazonaws.com/hifi-content/eric/Sounds/VRVJ/rain.wav?v3";
-    soundCartridgeProps.userData = JSON.stringify(soundCartridgeUserData)
-    soundCartridges.push(Entities.addEntity(soundCartridgeProps));
-  }
-
-  function spawnVisualCartridges() {
-    var visualCartridgeScriptURL = Script.resolvePath("visualCartridgeEntityScripts/firePoiVisualCartridgeEntityScript.js?v1" + Math.random());
-
-    var visualCartridgeProps = {
-      type: "Model",
-      modelURL: "https://s3-us-west-1.amazonaws.com/hifi-content/jazmin/dev/_vrhackathon/torch_disk.fbx",
-      shapeType: "sphere",
-      name: "VRVJ-Visual-Cartridge",
-      dimensions: {
-        x: 0.2355,
-        y: 0.2355,
-        z: 0.0187
-      },
-      rotation: Quat.fromPitchYawRollDegrees(0, -17, 0),
-      damping: 1,
-      collidesWith: "",
-      angularDamping: 1,
-      color: {
-        red: 0,
-        green: 200,
-        blue: 10
-      },
-      dynamic: true,
-      position: {x:204.1109, y: 0.6746, z: 352.4536},
-      script: visualCartridgeScriptURL,
-    }
-    visualCartridges.push(Entities.addEntity(visualCartridgeProps));
-
-    visualCartridgeProps.script = Script.resolvePath("visualCartridgeEntityScripts/reactiveSkybox/reactiveSkyboxSunVisualCartridgeEntityScript.js?v1" + Math.random());
-    var cartridgeUserData = {};
-    cartridgeUserData.reactiveSkybox = VRVJSkybox;
-    visualCartridgeProps.userData = JSON.stringify(cartridgeUserData);
-    visualCartridgeProps.position = {x: 205.9184, y: 0.68, z: 352.3227};
-    visualCartridgeProps.modelURL = "https://s3-us-west-1.amazonaws.com/hifi-content/jazmin/dev/_vrhackathon/sun_disk.fbx";
-    visualCartridges.push(Entities.addEntity(visualCartridgeProps));
-
-
-    visualCartridgeProps.script = Script.resolvePath("visualCartridgeEntityScripts/reactiveSkybox/reactiveSkyboxRainVisualCartridgeEntityScript.js?v1" + Math.random());
-    visualCartridgeProps.modelURL = "https://s3-us-west-1.amazonaws.com/hifi-content/jazmin/dev/_vrhackathon/rain_disk.fbx";
-    visualCartridgeProps.position = {
-      x: 205.0273,
-      y: 0.67,
-      z: 352.27
-    };
-    visualCartridges.push(Entities.addEntity(visualCartridgeProps));
 
 
   }
